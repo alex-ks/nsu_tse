177c02ea7f6471d0186b7e8165ba07a96fd8c1fb
Alexander Komissarov <a.komissarov@g.nsu.ru>
2017-09-28 19:28:08 +0700

    Now parallel methods extend basic class

--- ruby/task2.rb
+++ ruby/task2.rb
@@ -11,83 +11,58 @@
 
 require 'thread'
 
-class ParallelArray
+class Array
     @@threadCount = 4
 
 private
     def doWork(mapFunc, localReduce, globalReduce)
-        chunkSize = (@array.length / Float(@count)).ceil
+        @count = @@threadCount
+        chunkSize = (length / Float(@count)).ceil
         results = (0...@count).map {
             |i|
             Thread.new {
                 start = i * chunkSize
-                finish = if (i + 1) * chunkSize < @array.length then 
+                finish = if (i + 1) * chunkSize < length then 
                             (i + 1) * chunkSize 
                          else 
-                            @array.length 
+                            length 
                          end
 
-                puts ["Chunk size:", finish - start, "Tid:", Thread.current.object_id].inspect
-
-                localReduce.call ((start...finish).map { |j| mapFunc.call(@array[j]) })
+                localReduce.call ((start...finish).map { |j| mapFunc.call(self[j]) })
             }
         }.map { |worker| worker.value }
         globalReduce.call(results)
     end
 
 public
-    def ParallelArray.threadCount
+    def Array.threadCount
         @@threadCount
     end
 
-    def ParallelArray.threadCount=(value)
+    def Array.threadCount=(value)
         @@threadCount = value
     end
 
-    def threadCount
-        @count
-    end
-
-    def threadCount=(value)
-        @count = value
-    end
-
-    def value
-        @array
-    end
-
-    def inspect
-        @array.inspect
-    end
-
-    def initialize(array)
-        if not array.is_a?(Array)
-            raise ArgumentError, "Array must be provided"
-        end
-        @array = array
-        @count = @@threadCount
-    end
-
-    def map(&mapFunc)
+    def map!(&mapFunc)
         localReduce = lambda { |chunk| chunk }
-        globalReduce = lambda { |chunks| ParallelArray.new(chunks.reduce([]) { |accum, elem| accum + elem }) }
+        globalReduce = lambda { |chunks| chunks.reduce([]) { |accum, elem| accum + elem } }
         doWork(mapFunc, localReduce, globalReduce)
     end
 
-    def select()
+    def select!()
         mapFunc = lambda { |elem| [yield(elem), elem] }
         localReduce = lambda { |chunk| chunk.select { |tuple| tuple[0] }.map { |tuple| tuple[1] } }
-        globalReduce = lambda { |chunks| ParallelArray.new(chunks.reduce([]) { |accum, elem| accum + elem }) }
+        globalReduce = lambda { |chunks| chunks.reduce([]) { |accum, elem| accum + elem } }
         doWork(mapFunc, localReduce, globalReduce)
     end
 
-    def any?(&cond)
+    def any!(&cond)
         localReduce = lambda { |chunk| chunk.any? }
         globalReduce = lambda { |chunk| chunk.any? }
         doWork(cond, localReduce, globalReduce)
     end
 
-    def all?(&cond)
+    def all!(&cond)
         localReduce = lambda { |chunk| chunk.all? }
         globalReduce = lambda { |chunk| chunk.all? }
         doWork(cond, localReduce, globalReduce)
@@ -95,10 +70,12 @@ public
 
 end
 
-ma = ParallelArray.new((1..30).to_a)
+ma = (1..30).to_a
+mb = [0, 1, 2, 3, 5, 6, 7]
 
-puts ma.map {|i| i * 2 }.inspect
-puts ma.select {|i| i % 2 == 0 }.inspect
-puts ma.any? {|i| i % 2 == 0 }.inspect
-puts ma.all? {|i| i % 2 == 0 }.inspect
-puts ma.all? {|i| i % 1 == 0 }.inspect
+puts ma.map! { |i| i * 2 }.inspect
+puts ma.select! { |i| i % 2 == 0 }.inspect
+puts ma.any! { |i| i % 2 == 0 }.inspect
+puts ma.all! { |i| i % 2 == 0 }.inspect
+puts ma.all! { |i| i % 1 == 0 }.inspect
+puts mb.map! { |i| i * 3 }.inspect
